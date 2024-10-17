89a60da060f38ba45057f9b0eef31e5ae7d0e7b7
Nisha <nishabk@mit.edu>
2024-10-17 19:38:19 -0400

    Transferred concepts to new codebase

--- server/app.ts
+++ server/app.ts
@@ -2,10 +2,17 @@ import AuthenticatingConcept from "./concepts/authenticating";
 import FriendingConcept from "./concepts/friending";
 import PostingConcept from "./concepts/posting";
 import SessioningConcept from "./concepts/sessioning";
-
+import ReactionConcept from "./concepts/reaction";
+import FeedConcept from "./concepts/feed";
+import NotificationConcept from "./concepts/notification";
+import ReportingConcept from "./concepts/reporting";
 // The app is a composition of concepts instantiated here
 // and synchronized together in `routes.ts`.
 export const Sessioning = new SessioningConcept();
 export const Authing = new AuthenticatingConcept("users");
 export const Posting = new PostingConcept("posts");
 export const Friending = new FriendingConcept("friends");
+export const Reaction = new ReactionConcept("reactions");
+export const Feed = new FeedConcept("feeds");
+export const Notification = new NotificationConcept("notifications");
+export const Reporting = new ReportingConcept("reports");
